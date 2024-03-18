2d5d44e6256a5ad7b054d45f5a1a29eedca91da2
KutsunaSubaRya <index20010928@gmail.com>
2024-03-18 16:03:30 +0800

    feat: add lobby border wall

--- src/building/dungeon/area_gen.py
+++ src/building/dungeon/area_gen.py
@@ -3,11 +3,18 @@ from gdpc import WorldSlice
 from gdpc.vector_tools import Rect
 import time
 
+lobby_x = 50
+lobby_y = 0
+lobby_z = 50
+lobby_width_1 = 120
+lobby_width_2 = 120
+lobby_height = 12
+
 
 def wall_generate(editor: Editor, x_corner, y_height, z_corner, width_1, width_2, height):
     cube_generate(editor, x_corner, y_height, z_corner, width_1, width_2, height)
     cube_generate(editor, x_corner + 1, y_height + 1, z_corner + 1, width_1 - 2, width_2 - 2, height - 1)
-    no_border_cube_generate(editor, x_corner, y_height + height, z_corner, width_1, width_2, height, "gold_block")
+    no_border_cube_generate(editor, x_corner, y_height + height, z_corner, width_1, width_2, height, "air")
     for i in range(0, width_1 + 1):
         if i % 2 == 0:
             editor.runCommand(f"setblock {x_corner + i} {y_height + height} {z_corner} stone_bricks")
@@ -36,6 +43,11 @@ def no_border_cube_generate(editor: Editor, x_corner, y_height, z_corner, width_
         editor.flushBuffer()
 
 
+def lobby_generate(editor: Editor, x_corner, y_height, z_corner, width_1, width_2, height):
+    cube_generate(editor, x_corner - 3, y_height, z_corner - 3, width_1 + 6, width_2 + 6, height * 2 + 1)
+    wall_generate(editor, lobby_x, lobby_y, lobby_z, lobby_width_1, lobby_width_2, lobby_height)
+
+
 def get_height(worldSlice: WorldSlice, x, z):
     """
     Get the height of the worldSlice at the given x and z coordinates.
@@ -58,4 +70,4 @@ if __name__ == "__main__":
     editor = Editor()
     # height1 = get_height(editor.worldSlice, -87, 102)
     # print(height1)
-    # wall_generate(editor, 1300, 0, 1300, 120, 120, 12)
+    lobby_generate(editor, lobby_x, lobby_y, lobby_z, lobby_width_1, lobby_width_2, lobby_height)
